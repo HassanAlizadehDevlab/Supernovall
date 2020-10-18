167eec760f3b0a8558b963a9791ebb1847f3a167
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 03:20:25 +0330

    Remove redundant classes and modules

--- common_test/src/test/java/com/android/common_test/MockitoExt.kt
+++ common_test/src/test/java/com/android/common_test/MockitoExt.kt
@@ -5,11 +5,6 @@ import androidx.lifecycle.LiveData
 /**
  * a kotlin friendly mock that handles generics
  */
-fun <T> LiveData<T>.observeOnce(onChangeHandler: (T) -> Unit) {
-    val observer = OneTimeObserver(handler = onChangeHandler)
-    observe(observer, observer)
-}
-
 fun <T> LiveData<T>.observe(onChangeHandler: (T) -> Unit) {
     val observer = UnlimitedObserver(handler = onChangeHandler)
     observe(observer, observer)
