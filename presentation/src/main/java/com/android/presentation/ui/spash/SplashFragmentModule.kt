f3d231196bb76dabb7c14a4d9488a6cd77937f92
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 02:47:16 +0330

    Provide objects in splash fragment scope

---
+++ presentation/src/main/java/com/android/presentation/ui/spash/SplashFragmentModule.kt
@@ -0,0 +1,9 @@
+package com.android.presentation.ui.spash
+
+import dagger.Module
+
+/**
+ * Created by hassanalizadeh on 19,October,2020
+ */
+@Module(includes = [SplashViewModelModule::class])
+abstract class SplashFragmentModule
---
+++ presentation/src/main/java/com/android/presentation/ui/spash/SplashViewModelModule.kt
@@ -0,0 +1,20 @@
+package com.android.presentation.ui.spash
+
+import androidx.lifecycle.ViewModel
+import com.android.presentation.common.di.FragmentScope
+import com.android.presentation.common.di.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+/**
+ * Created by hassanalizadeh on 19,October,2020
+ */
+@Module
+abstract class SplashViewModelModule {
+    @Binds
+    @IntoMap
+    @FragmentScope
+    @ViewModelKey(SplashViewModel::class)
+    abstract fun repositoryViewModel(viewModel: SplashViewModel): ViewModel
+}
