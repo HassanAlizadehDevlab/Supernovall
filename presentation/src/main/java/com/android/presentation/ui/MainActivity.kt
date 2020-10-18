93deac09850370f9dcd2a6d7e0ce0a72ca38193d
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-18 19:03:03 +0330

    Implement basis of Project

---
+++ presentation/src/main/java/com/android/presentation/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.android.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
