c603769cf98cab979b72128f20104d75d73845d9
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 02:33:29 +0330

    Clean the base classes for view model and activity

--- presentation/src/main/java/com/android/presentation/common/view/BaseActivityModule.kt
+++ presentation/src/main/java/com/android/presentation/common/view/BaseActivityModule.kt
@@ -1,11 +1,10 @@
 package com.android.presentation.common.view
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import com.android.presentation.common.di.ActivityScope
 import com.android.presentation.common.viewmodel.ViewModelProviderFactory
+import com.android.presentation.ui.MainActivity
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -13,10 +12,6 @@ import javax.inject.Named
 
 @Module
 abstract class BaseActivityModule {
-
-    @Binds
-    @ActivityScope
-    abstract fun activityContext(appCompatActivity: AppCompatActivity): Context
 
     /**
      * Bind it just once
@@ -35,8 +30,8 @@ abstract class BaseActivityModule {
         @Provides
         @Named(ACTIVITY_FRAGMENT_MANAGER)
         @ActivityScope
-        fun provideActivityFragmentManager(appCompatActivity: AppCompatActivity): FragmentManager {
-            return appCompatActivity.supportFragmentManager
+        fun provideActivityFragmentManager(activity: MainActivity): FragmentManager {
+            return activity.supportFragmentManager
         }
     }
 
