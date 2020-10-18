96f365859d9280de0104ae8f39027e00699ce91b
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 02:35:45 +0330

    Provide city repository object

--- data/src/main/java/com/android/data/repository/RepositoryModule.kt
+++ data/src/main/java/com/android/data/repository/RepositoryModule.kt
@@ -1,10 +1,21 @@
 package com.android.data.repository
 
 import com.android.data.repository.datasource.DataSourceModule
+import com.android.domain.repository.CityRepository
+import dagger.Binds
 import dagger.Module
+import dagger.Reusable
 
 /**
  * Created by hassanalizadeh on 19,September,2020
  */
 @Module(includes = [DataSourceModule::class])
-abstract class RepositoryModule
+abstract class RepositoryModule {
+
+    @Binds
+    @Reusable
+    abstract fun provideCityRepository(
+        cityRepositoryImpl: CityRepositoryImpl
+    ): CityRepository
+
+}
