bb13fd81a85ff5ab7373ecc9a2ffd670880d31ca
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-18 19:26:28 +0330

    Provide context in activity scope

--- presentation/src/main/java/com/android/presentation/ui/MainActivityModule.kt
+++ presentation/src/main/java/com/android/presentation/ui/MainActivityModule.kt
@@ -1,9 +1,19 @@
 package com.android.presentation.ui
 
+import android.content.Context
+import com.android.presentation.common.di.ActivityScope
+import dagger.Binds
 import dagger.Module
 
 /**
  * Created by hassanalizadeh on 18,October,2020
  */
 @Module
-abstract class MainActivityModule
+abstract class MainActivityModule {
+
+    @Binds
+    @ActivityScope
+    abstract fun activityScope(activity: MainActivity): Context
+
+
+}
