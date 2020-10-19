9295fc046a04f33b23f6cd57aefaa177e39d3a4a
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 03:33:31 +0330

    Provide city view model module

---
+++ presentation/src/main/java/com/android/presentation/ui/city/CityViewModelModule.kt
@@ -0,0 +1,20 @@
+package com.android.presentation.ui.city
+
+import androidx.lifecycle.ViewModel
+import com.android.presentation.common.di.FragmentScope
+import com.android.presentation.common.di.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+/**
+ * Created by hassanalizadeh on 19,October,2020
+ */
+@Module
+abstract class CityViewModelModule {
+    @Binds
+    @IntoMap
+    @FragmentScope
+    @ViewModelKey(CityViewModel::class)
+    abstract fun repositoryViewModel(viewModel: CityViewModel): ViewModel
+}
