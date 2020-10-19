6eae70b81a656bf7766d18d5fb0439554b5b973e
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-19 03:33:55 +0330

    Include view model in city fragment module

---
+++ presentation/src/main/java/com/android/presentation/ui/city/CityFragmentModule.kt
@@ -0,0 +1,9 @@
+package com.android.presentation.ui.city
+
+import dagger.Module
+
+/**
+ * Created by hassanalizadeh on 19,October,2020
+ */
+@Module(includes = [CityViewModelModule::class])
+abstract class CityFragmentModule
